1731700943f73a9de7997c53c747e66e32a85130
khadijaes-sarghini-cmyk <khadija.es-sarghini@uit.ac.ma>
2025-10-27 20:52:03 +0100

    refactor PGCD calculation and update I/o mrthods
    Refactor PGCD calculation to use Euclid's algorithm in a separate function. Update input/output to use printf and scanf instead of iostream.

--- pgcd.cpp
+++ pgcd.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+
+// Fonction qui calcule le PGCD avec l'algorithme d'Euclide 
+int pgcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;  // Retourne la valeur absolue si elle est négative 
+}
 
 int main() {
-    int a, b;
-    cout << "Entrez deux entiers positifs : ";
-    cin >> a >> b;
-
-    int x = a;
-    int y = b;
-
-    while (y != 0) {
-        int r = x % y;
-        x = y;
-        y = r;
-    }
-
-    cout << "Le PGCD de " << a << " et " << b << " est : " << x << endl;
-
+    int num1, num2;
+    
+    printf("veuillez saisir un nombre num1: ");
+    scanf("%d", &num1);
+    
+    printf("veuillez saisir un nombre num2: ");
+    scanf("%d", &num2);
+    
+    if (num1 < 0) num1 = -num1; // prendre la valeur absolue 
+    if (num2 < 0) num2 = -num2;
+    
+    int resultat = pgcd(num1, num2);
+    
+    printf("PGCD de %d et %d est : %d\n", num1, num2, resultat);
+    
     return 0;
 }
