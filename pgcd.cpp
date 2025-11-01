057848094daab7fdf6584549447c360416a6d725
khadijaes-sarghini-cmyk <khadija.es-sarghini@uit.ac.ma>
2025-11-01 15:02:09 +0100

    j'ai ajoutée au projet les étapes de l'algorithme d'Euclide et la détection des ombres premiers
    Updated PGCD program to enhance user interaction and display steps of the Euclidean algorithm.

--- pgcd.cpp
+++ pgcd.cpp
@@ -1,30 +1,26 @@
 #include <stdio.h>
 
-// Fonction qui calcule le PGCD avec l'algorithme d'Euclide 
-int pgcd(int a, int b) {
+int main() {
+    int a, b, temp;
+    
+    printf("Entrez deux entiers : ");
+    scanf("%d%d", &a, &b);
+
+    int x = a, y = b;
+
+    printf("Étapes de l'algorithme d'Euclide :\n");
     while (b != 0) {
-        int temp = b;
+        printf("%d = %d * (%d) + %d\n", a, a / b, b, a % b);
+        temp = b;
         b = a % b;
         a = temp;
     }
-    return a;  // Retourne la valeur absolue si elle est négative 
-}
 
-int main() {
-    int num1, num2;
-    
-    printf("veuillez saisir un nombre num1: ");
-    scanf("%d", &num1);
-    
-    printf("veuillez saisir un nombre num2: ");
-    scanf("%d", &num2);
-    
-    if (num1 < 0) num1 = -num1; // prendre la valeur absolue 
-    if (num2 < 0) num2 = -num2;
-    
-    int resultat = pgcd(num1, num2);
-    
-    printf("PGCD de %d et %d est : %d\n", num1, num2, resultat);
-    
+    printf("PGCD = %d\n", a);
+
+    if (a == 1) {
+        printf("Les deux nombres sont premiers entre eux.\n");
+    }
+
     return 0;
 }
